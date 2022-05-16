e63f44b2cfd779ae49b016fec258cf07d1105058
liji356 <lijibthomas95@gmail.com>
2022-05-16 20:18:08 +0530

    search com

--- src/App.js
+++ src/App.js
@@ -7,33 +7,18 @@ import Addstud from './component/Addstud';
 function App() {
   return (
     
-<div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row g-3">
-                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label class="label-form">Name</label>
-                    <input type="text" class="form-control"/>
-                </div>
-                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" class="form-label">Roll No</label>
-                    <input type="text" class="form-control"/>
-                </div>
-                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" class="form-form">Admission No</label>
-                    <input type="text" class="form-control"/>
-                </div>
-                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" class="form-label">University No</label>
-                    <input type="text" class="form-control"/>
-                </div>
-                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label for="" class="form-label">College</label>
-                    <input type="text" class="form-control"/>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-012">
 
+
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">University No</label>
+                    <input type="text" className="form-control"/>
                 </div>
-                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button class="btn btn-info">REGISTER</button>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-info">Search</button>
                 </div>
             </div>
         </div>
