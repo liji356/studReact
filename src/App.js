93a7e6713a922d31dfc7016d9e3e5d2e1813589c
liji356 <lijibthomas95@gmail.com>
2022-05-17 11:25:34 +0530

    navbarcomplete

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addstud from './component/Addstud';
 import Serach from './component/Serach';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
 
 <>
-<Addstud/>
-<Serach/>
+<BrowserRouter>
+<Routes>
+<Route path="/" exact element={<Addstud/>}/>
+<Route path="/search" exact element={<Serach/>}/>
+</Routes>
+</BrowserRouter>
 </>
  
 
